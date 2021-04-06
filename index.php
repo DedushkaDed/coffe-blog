23f4d2eb17f545f86ff08b638019184ca09826e1
= <glebstuck@yandex.ru>
2021-04-06 23:05:53 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php"); ?>
+
+<p> Контент 123</p>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/blog/header.php
@@ -0,0 +1,2 @@
+
+<p>Eto header!</p>
